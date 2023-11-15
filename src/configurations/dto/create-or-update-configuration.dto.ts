6598bab9c53e26971e28e8b150bcff20aa440b36
silasoliv39 <silasoliv39@gmail.com>
2023-11-15 20:23:00 -0300

    fix: adding validation in dto

--- src/configurations/dto/create-or-update-configuration.dto.ts
+++ src/configurations/dto/create-or-update-configuration.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNumber, IsOptional } from 'class-validator';
+import {
+  IsBoolean,
+  IsIn,
+  IsNumber,
+  IsOptional,
+  ValidateIf,
+} from 'class-validator';
 
 export class CreateOrUpdateConfigurationDto {
   @ApiProperty({ required: false })
@@ -14,6 +20,8 @@ export class CreateOrUpdateConfigurationDto {
 
   @ApiProperty({ required: false })
   @IsOptional()
+  @ValidateIf((object) => object.font_size != null)
   @IsNumber()
+  @IsIn([1.5, 2, 2.5], { message: 'The value must be 1.5, 2, 2.5' })
   font_size?: number | null;
 }
